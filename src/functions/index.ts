c7c15eb36a8b6e9dc89b4e325a12456fc3b772d2
André Brandão <andrenbrandao@gmail.com>
2021-01-30 20:01:51 -0300

    feat: update note

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,3 +2,4 @@ export { default as hello } from './hello';
 export { default as create } from './create';
 export { default as get } from './get';
 export { default as list } from './list';
+export { default as update } from './update';
